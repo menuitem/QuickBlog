f5729a1e2e49237d02d0d068814799dfc63fb63c
Lucjan Stepien <lucjanstepien@gmail.com>
2015-01-11 21:08:55 +0000

    rails bower configuration

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module QuickBlog
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
